5e692fd3038773cb3c11016fab2e14ab24a58104
Adilson Gabriel <adilsongb.rabelo@gmail.com>
2023-10-15 13:59:32 -0300

    chore: config project imports and libs

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@styles': path.resolve(__dirname, 'src/styles'),
+      '@components': path.resolve(__dirname, 'src/components'),
+    },
+  },
+};
